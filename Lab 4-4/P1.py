4628db3cf3471d58e148c2fcea3721d7a9638fe2
JiravitB <jiravit.b@kkumail.com>
2026-01-15 10:59:15 +0700

    4-4

--- Lab 4-4/P1.py
+++ Lab 4-4/P1.py
@@ -1,23 +1,25 @@
-from abc import ABC, abstractmethod
+from room import Bedroom, Kitchen
 
-class Room(ABC):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-    
-    @abstractmethod
-    def get_purpose(self):
-        """Returns a string describing purposes of the room"""
-        pass
+bed = Bedroom(10,20,5)
+print(bed.describe_room())
+print(f"Bed size : {bed.bed_size} ft")
+print(f"Reccommended lighting : {bed.get_recommended_lighting()} lumen / square foot ")
+print()
 
-    @abstractmethod
-    def get_recommended_lighting(self):
-        """Returns recommended lighting in lumens per square foot"""
-        pass
+K_no = Kitchen(15,25)
+print("Kitchen with island")
+print(K_no.describe_room())
+print(f"Reccommended lighting : {K_no.get_recommended_lighting()} lumen / square foot ")
+island1,wall1 = K_no.calculate_counter_space()
+print(f"Counter space : Wall {wall1}, Island {island1}")
+print()
 
-    def calculate_area(self):
-        return self.length * self.width
-    
-    def describe_room(self):
-        area = self.calculate_area()
-        return f"A {self.__class__.__name__} of {area} sq ft used for {self.get_purpose()}"
+K_yes = Kitchen(15,25,False)
+print("Kitchen without island")
+print(K_yes.describe_room())
+print(f"Reccommended lighting : {K_yes.get_recommended_lighting()} lumen / square foot ")
+island2,wall2 = K_yes.calculate_counter_space()
+print(f"Counter space : Wall {wall2}, Island {island2}")
+print()
+
+print(K_yes.calculate_counter_space.__doc__)
---
+++ Lab 4-4/room.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+
+class Room(ABC):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    @abstractmethod
+    def get_purpose(self):
+        """Returns a string describing purposes of the room"""
+        pass
+
+    @abstractmethod
+    def get_recommended_lighting(self):
+        """Returns recommended lighting in lumens per square foot"""
+        pass
+
+    def calculate_area(self):
+        return self.length * self.width
+    
+    def describe_room(self):
+        area = self.calculate_area()
+        return f"A {self.__class__.__name__} of {area} sq ft used for {self.get_purpose()}"
+
+class Bedroom(Room):
+    def __init__(self, length, width, bed_size):
+        super().__init__(length, width)
+        self.bed_size = bed_size
+
+    def get_purpose(self):
+        return "Sleep"
+
+    def get_recommended_lighting(self):
+        return 20 #lumens per square foot
+
+class Kitchen(Room):
+    def __init__(self, length, width, has_island=True):
+        super().__init__(length, width)
+        self.has_island = has_island
+
+    def get_purpose(self):
+        return "Cook"
+
+    def get_recommended_lighting(self):
+        return  40 #lumens per square foot
+
+    def calculate_counter_space(self):
+        
+        """ calculate counter space
+        
+        Args : none
+
+        Return :
+            float : island (if island is False it will be 0)
+            float : wall
+
+        Raises : none
+
+        Examples :
+                >>> calculate_counter_space() (length = 15, widht = 25, Island = True)
+                Island = 75.0
+                Wall = 93.75
+
+        """
+
+
+        area = self.calculate_area()
+
+        if self.has_island:
+            island = area / 5
+            wall = area / 4
+
+        else:
+            island = 0
+            wall = area / 2
+
+        return (island, wall)
