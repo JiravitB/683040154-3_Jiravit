44aeaf1fc043e61c47ea5b533e8b92ebd51cc614
JiravitB <jiravit.b@kkumail.com>
2026-02-11 23:23:25 +0700

    Update P1.py

--- Lab 5-1/P1.py
+++ Lab 5-1/P1.py
@@ -18,13 +18,34 @@ class login(QWidget):
         super().__init__()
         
         main_layout = QVBoxLayout()
+        button_layout = QHBoxLayout()
 
         title = QLabel("LOGIN")
         main_layout.addWidget(title)
 
-        
+        main_layout.addWidget(QLabel("Email"))
+        self.email = QLineEdit()
+        main_layout.addWidget(self.email)
 
+        main_layout.addWidget(QLabel("Password"))
+        self.Pass = QLineEdit()
+        main_layout.addWidget(self.Pass)
+
+        self.rem = QCheckBox("Remember me?")
+        main_layout.addWidget(self.rem)
+
+        self.log = QPushButton("LOGIN")
+        self.log.setFixedWidth(150)
+        button_layout.addWidget(self.log)
+        button_layout.setAlignment(Qt.AlignCenter)
+        
+        main_layout.addLayout(button_layout)
         self.setLayout(main_layout)
+
+        self.forgot = QLabel("Forgot password?")
+        self.forgot.setAlignment(Qt.AlignRight)
+        main_layout.addWidget(self.forgot)
+
 
 
 if __name__ == "__main__":
@@ -33,7 +54,7 @@ if __name__ == "__main__":
     window = QMainWindow()
     window.setWindowTitle("P1: Login")
     window.setCentralWidget(login())
-    window.setFixedSize(400, 600)
+    window.setFixedSize(400, 500)
     window.show()
 
     sys.exit(app.exec())
