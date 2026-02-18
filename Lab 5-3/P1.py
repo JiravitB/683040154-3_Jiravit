bfbcd7104ebb96872fcda9238180a37cada7253b
JiravitB <jiravit.b@kkumail.com>
2026-02-19 02:06:06 +0700

    Update P1.py

--- Lab 5-3/P1.py
+++ Lab 5-3/P1.py
@@ -15,10 +15,12 @@ class Student(QMainWindow):
         self.setCentralWidget(central_widget)
         main_layout = QVBoxLayout(central_widget)
 
+#create Layout
         input_layout = QHBoxLayout()
         score_layout = QHBoxLayout()
         button_layout = QHBoxLayout()
 
+#Student ID
         id = QLabel("Students ID: ")
         self.id = QComboBox()
         self.id.setEditable(True)
@@ -27,12 +29,14 @@ class Student(QMainWindow):
         input_layout.addWidget(id)
         input_layout.addWidget(self.id)
 
+#Student Name
         name = QLabel("Student Name: ")
         self.name = QLineEdit()
         
         input_layout.addWidget(name)
         input_layout.addWidget(self.name)
 
+#Math
         math = QLabel("Math: ")
         self.math = QSpinBox()
         self.math.setRange(0, 100)
@@ -41,6 +45,7 @@ class Student(QMainWindow):
         score_layout.addWidget(math)
         score_layout.addWidget(self.math)
 
+#Science
         sci = QLabel("Science: ")
         self.sci = QSpinBox()
         self.sci.setRange(0, 100)
@@ -49,6 +54,7 @@ class Student(QMainWindow):
         score_layout.addWidget(sci)
         score_layout.addWidget(self.sci)
 
+#English
         eng = QLabel("English: ")
         self.eng = QSpinBox()
         self.eng.setRange(0, 100)
@@ -57,19 +63,24 @@ class Student(QMainWindow):
         score_layout.addWidget(eng)
         score_layout.addWidget(self.eng)
 
+#Add student button
         self.add = QPushButton("Add Student")
         self.add.clicked.connect(self.add_student)
-        
+
+#Add reset button
         self.reset = QPushButton("Reset Input")
         self.reset.clicked.connect(self.reset_input)
 
+#Add clear button
         self.clear = QPushButton("Clear All")
         self.clear.clicked.connect(self.clear_all)
 
+#Add all button to button layout
         button_layout.addWidget(self.add)
         button_layout.addWidget(self.reset)
         button_layout.addWidget(self.clear)
 
+#Make table
         self.table = QTableWidget()
         self.table.setColumnCount(8)
         self.table.setHorizontalHeaderLabels(["Student ID", "Name", "Math", "Science", "English", "Total", "Average", "Grade"])
@@ -83,6 +94,7 @@ class Student(QMainWindow):
         self.table.setColumnWidth(5,70)
         self.table.setColumnWidth(6,70)
 
+#Add all layout to main layout
         main_layout.addLayout(input_layout)
         main_layout.addLayout(score_layout)
         main_layout.addLayout(button_layout)
@@ -95,10 +107,12 @@ class Student(QMainWindow):
         stu_sci = self.sci.value()
         stu_eng = self.eng.value()
 
+#Validate ID |ควรเช็คเลขด้วย|
         if not stu_id:
             QMessageBox.warning(self, "Input Error", "Please enter ID")
             return
 
+#validate name
         if not stu_name:
             QMessageBox.warning(self, "Input Error", "Please enter name")
             return
@@ -106,6 +120,7 @@ class Student(QMainWindow):
         row_position = self.table.rowCount()
         self.table.insertRow(row_position)
 
+#Make everything to Table Item
         id_item = QTableWidgetItem(stu_id)
         name_item = QTableWidgetItem(stu_name)
 
@@ -146,7 +161,7 @@ class Student(QMainWindow):
             grade = QTableWidgetItem("F")
             grade.setBackground(QColor("#FF3300"))
 
-
+#Add all Item to table
         self.table.setItem(row_position, 0, id_item)
         self.table.setItem(row_position, 1, name_item)
         self.table.setItem(row_position, 2, math_item)
